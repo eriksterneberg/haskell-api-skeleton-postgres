4add5d9e2e743266b55e3118f94df4a060e49fb3
Erik Sterneberg <erik.sterneberg@gmail.com>
2017-02-19 22:03:06 +0800

    Refactor

--- src/Config.hs
+++ src/Config.hs
@@ -1,4 +1,7 @@
-module Config where
+module Config ( getEnvironment
+              , getLogger
+              , getPort
+              ) where
 
 import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe, isNothing)
@@ -11,32 +14,32 @@ import Network.Wai.Middleware.RequestLogger
 type Port = Int
 
 
-defaultPort :: Port
-defaultPort = 3000
-
-
 data Environment = Development
                  | Production
                  | Test
                  deriving (Eq, Read, Show)
 
 
+-- Get system variable or use default Environment
 getEnvironment :: IO Environment
-getEnvironment = do
-    systemVal <- lookupEnv "ENVIRONMENT"
-    let environment = systemVal >>= \x -> readMaybe x :: Maybe Environment
-    when (isNothing environment) $ putStrLn "Set environment variable ENVIRONMENT=X to override default environment setting."
-    return $ fromMaybe Development $ environment
+getEnvironment = getDefault "ENVIRONMENT" readMaybe Development
 
 
+-- Get system environment variable or use default 3000
 getPort :: IO Port
-getPort = do
-    systemVal <- lookupEnv "PORT"
-    let port = systemVal >>= \x -> readMaybe x :: Maybe Int
-    when (isNothing port) $ putStrLn "Set environment PORT=X to override default port."
-    return $ fromMaybe defaultPort $ port
+getPort = getDefault "PORT" readMaybe 3000
 
 
+-- Get system environment variable or use default
+getDefault :: Show b => String -> (String -> Maybe b) -> b -> IO b
+getDefault env parser defaultVal = do
+    systemVal <- lookupEnv env
+    let port = systemVal >>= parser
+    when (isNothing port) $ putStrLn ("Using default value=" ++ show defaultVal ++ " Set environment PORT=X to override default.")
+    return $ fromMaybe defaultVal $ port
+
+
+-- Get request logger
 getLogger :: Environment -> Middleware
 getLogger Development = logStdoutDev
 getLogger Production  = logStdout
