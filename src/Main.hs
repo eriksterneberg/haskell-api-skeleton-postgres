b6b467d528d28f1c1d88640fe4fec9d872deefc2
Erik Sterneberg <erik.sterneberg@gmail.com>
2017-02-19 20:08:51 +0800

    Get port from command line

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Data.Text.Lazy (pack, intercalate)
 import Data.Monoid ((<>))
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.HttpAuth
+import Network.Wai (Middleware)
 import Data.ByteString (ByteString)
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
@@ -57,22 +58,41 @@ defaultPort :: Port
 defaultPort = 3000
 
 
+data Environment = Development
+                 | Production
+                 | Test
+                 deriving (Eq, Read, Show)
+
+
+getEnvironment :: IO Environment
+getEnvironment = fmap (maybe Development read) (lookupEnv "ENVIRONMENT")                 
+
+
 getPort :: IO Port
 getPort = do
     systemVal <- lookupEnv "PORT"
-    let port = case systemVal of Nothing -> defaultPort
-                                 Just stringVal -> case readMaybe stringVal :: Maybe Int of Nothing -> defaultPort
-                                                                                            Just intVal -> intVal
-    return port
+    case systemVal of
+        Nothing -> do
+            putStrLn "No PORT suppplied on command line; using default."
+            return defaultPort
+        Just stringVal -> case readMaybe stringVal :: Maybe Int of 
+            Nothing -> putStrLn (stringVal ++ " is not a valid integer to be used as port.") >> return defaultPort
+            Just intVal -> return intVal
+
+
+getLogger :: Environment -> Middleware
+getLogger Development = logStdoutDev
+getLogger Production  = logStdout
+getLogger _           = logStdout  -- Todo: switch to no log
 
 
 main :: IO ()
 main = do
     putStrLn "Starting server..."
     port <- getPort
+    environment <- getEnvironment
     scotty port $ do
-        -- middleware logStdout
-        middleware logStdoutDev
+        middleware $ getLogger environment
         middleware $ basicAuth authenticate "Default Realm"
         routes
 
