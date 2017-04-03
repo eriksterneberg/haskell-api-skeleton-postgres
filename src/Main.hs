ce185fb4bb451dff4311b18bea849c6bcab631fe
Erik Sterneberg <erik.sterneberg@gmail.com>
2017-04-04 01:02:06 +0800

    WIP: Can save and retrieve from Postgres db; migrations

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,26 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Main where
+
+import GHC.Generics
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (pack)
+import Data.Text.Lazy (fromStrict)
+import qualified Web.Scotty as Scotty
+
+import Control.Monad.IO.Class  (liftIO)
+import qualified Database.Persist.Postgresql as DB
+import Control.Monad.Logger (runStderrLoggingT)
+import Control.Monad.Trans.Reader (ReaderT)
 
 import Web.Scotty
 import Network.Wai.Middleware.HttpAuth
@@ -9,6 +29,8 @@ import Data.ByteString (ByteString)
 import qualified Config
 import qualified Router
 
+import qualified User
+
 
 authenticate :: ByteString -> ByteString -> IO Bool
 authenticate user password = return (user == "user@email.com" && password == "foobar")
@@ -16,7 +38,24 @@ authenticate user password = return (user == "user@email.com" && password == "fo
 
 main :: IO ()
 main = do
-    putStrLn "Starting server..."
+    startServer
+
+
+runDb pool query = liftIO (DB.runSqlPool query pool)
+
+
+-- instance ToJSON User.UserModel
+-- instance FromJSON User.UserModel
+
+
+startServer :: IO()
+startServer = do
+    putStrLn "Initializing db..."
+    pool <- runStderrLoggingT $ DB.createPostgresqlPool connStr 10
+    runDb pool doMigrations
+    -- runDb pool addTestData
+
+    putStrLn "Starting Scotty server..."
 
     -- Port for server to run on
     port <- Config.getPort
@@ -30,7 +69,36 @@ main = do
         middleware $ Config.getLogger environment
 
         -- Authenticate request to service
-        middleware $ basicAuth authenticate "Default Realm"
+        -- middleware $ basicAuth authenticate "Default Realm"
 
         -- Routes
-        Router.routes
+        -- Test, get all
+        get "/" $ do
+            allUsers <- runDb pool (DB.selectList [] [])
+            -- liftIO $ print allUsers
+            -- Scotty.json User.allUsers
+            Scotty.json (allUsers :: [DB.Entity User.UserModel])
+
+            -- Working
+            -- Scotty.text $ fromStrict $ pack $ show (allUsers :: [DB.Entity User.UserModel])
+
+        -- Other routes
+        -- Router.routes
+
+
+ -- transformers-0.5.2.0:Control.Monad.Trans.Reader.ReaderT
+                        -- DB.SqlBackend IO ()
+
+addTestData :: ReaderT DB.SqlBackend IO ()                        
+addTestData = do
+    erikId <- DB.insert (User.UserModel "erik.sterneberg@foo.com" Nothing (Just 34))
+    erik <- DB.get erikId
+    liftIO $ print erik                        
+
+
+
+connStr :: DB.ConnectionString
+connStr = "host=localhost dbname=user user=postgres password=postgres port=5432"
+
+
+doMigrations = DB.runMigration User.migrateAll
--- src/apps/User.hs
+++ src/apps/User.hs
@@ -1,13 +1,137 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module User where
 
+-- ( initDb
+--             , routes
+--             , Person
+--             ) where
+
 import GHC.Generics
-import Web.Scotty
-import Network.HTTP.Types.Method (StdMethod(..))
 import Data.Aeson (FromJSON, ToJSON)
 
+-- Imports Scotty
+import qualified Web.Scotty as Scotty
+import Network.HTTP.Types.Method (StdMethod(..))
+
+-- Imports for Database
+-- import Control.Monad.IO.Class  (liftIO)
+-- import Database.Persist
+import Database.Persist.Postgresql
+import Database.Persist.TH
+-- import Control.Monad.Logger (runStderrLoggingT)
+
+-- App imports
 import Types
+
+
+-- DB
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+UserModel json
+    email String
+    name String Maybe
+    age Int Maybe
+    deriving Show
+|]
+
+
+-- instance ToJSON UserModel
+-- instance FromJSON UserModel
+
+-- instance ToJSON Entity UserModel
+-- instance FromJSON Entity UserModel
+
+
+-- main' :: IO ()
+-- main' = do
+--     dbFunction doMigrations
+--     -- dbFunction doDbStuff
+
+-- doMigrations = runMigration migrateAll
+
+
+-- inHandlerDb = liftIO . User.dbFunction
+
+-- doDbStuff = do
+--         johnId <- insert $ Person "John Doe" $ Just 35
+
+--         john <- get johnId
+--         liftIO $ print (john :: Maybe Person)
+
+
+-- dbFunction query = runStderrLoggingT $ 
+--         withPostgresqlPool connStr 10 $ 
+--         \pool -> liftIO $ runSqlPersistMPool query pool          
+
+-- initDb :: IO ()
+-- initDb = runSqlite "dev.db" $ do
+    -- liftIO $ putStrLn "Inializing database for User app"
+--     doMigration
+--     initTestData  
+
+
+-- initTestData = do
+--     johnId <- insert $ Person "John Doe" $ Just 35
+--     john <- get johnId
+--     liftIO $ print john    
+
+--     -- oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
+--     -- liftIO $ print (oneJohnPost :: [Entity BlogPost])
+--     liftIO $ print (john :: Maybe Person)
+
+
+-- initTestData :: SqlBackend
+-- initTestData = do
+--     erikId <- insert (UserModel "erik.sterneberg@foo.com" Nothing (Just 34))
+--     erik <- get erikId
+--     liftIO $ print erik
+
+    -- johnId <- insert $ User "John Doe" $ Just 35
+--     john <- get johnId
+--     liftIO $ print john    
+
+--     -- oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
+--     -- liftIO $ print (oneJohnPost :: [Entity BlogPost])
+--     liftIO $ print (john :: Maybe Person)
+
+
+
+ -- = runSqlite "dev.db" $ do
+
+--     erikId <- insert $ Person "Erik" $ Just 35
+--     post <- selectList [] [LimitTo 1]
+--     liftIO $ print (port :: [Person])
+
+-- select' = runSqlite "dev.db" $ do
+--     people <- selectList [PersonAge >. 25, PersonAge <=. 30] []
+--     liftIO $ print people
+
+
+-- initDb :: IO ()
+-- initDb = withSqlitePool "test.db3" 10 $ runSqlPool doMigration'
+
+
+--     erikId <- insert $ Person "Erik" $ Just 35
+--     post <- selectList [] [LimitTo 1]
+--     liftIO $ print (port :: [Person])
+
+-- select' = runSqlite "dev.db" $ do
+--     people <- selectList [PersonAge >. 25, PersonAge <=. 30] []
+--     liftIO $ print people
+
+
+-- END DB 
 
 
 data User = User { userId :: Int
@@ -40,17 +164,17 @@ matchesId id' user = userId user == id'
 routes :: [Route]
 routes =
     [ 
-      Route GET "/user" $ json allUsers  -- all users
+      Route GET "/user" $ Scotty.json allUsers  -- all users
 
       -- Route to get user using a certain id
     , Route GET "/user/:id" (do
-        id' <- param "id"
-        json (filter (matchesId id') allUsers)) -- one user
+        id' <- Scotty.param "id"
+        Scotty.json (filter (matchesId id') allUsers)) -- one user
 
       -- Route to create new user
     , Route POST "/user" (do
-        user <- jsonData :: ActionM User
-        json user)
+        user <- Scotty.jsonData :: Scotty.ActionM User
+        Scotty.json user)
 
       -- Route to update user
     -- , Route PUT "/user" (do
